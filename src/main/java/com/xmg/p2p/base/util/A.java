a08c787eff29450fab6ed9ee0bfbc054ec437088
shlvbx <lvbx_001@foxmail.com>
2017-09-29 23:20:28 +0800

    修改

--- src/main/java/com/xmg/p2p/base/util/A.java
+++ src/main/java/com/xmg/p2p/base/util/A.java
@@ -5,11 +5,8 @@ public class A {
 		System.out.println("abcdef");
 
           System.out.println("1234");
-          System.out.println("4567");
           System.out.println("aaaaa");
           System.out.println("bbbb");
-
-              
 		 System.out.println("4567");
 		 System.out.println("DDDD");
 
