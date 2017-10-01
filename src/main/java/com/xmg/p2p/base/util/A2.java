24a59bc372cf07d640f2b848e653ef2a29f505a2
shlvbx <lvbx_001@foxmail.com>
2017-10-01 14:53:52 +0800

    添加A2类

---
+++ src/main/java/com/xmg/p2p/base/util/A2.java
@@ -0,0 +1,17 @@
+package com.xmg.p2p.base.util;
+
+public class A2 {
+	public static void main(String[] args) {
+		System.out.println("abcdef");
+
+          System.out.println("1234");
+          System.out.println("aaaaa");
+          System.out.println("bbbb");
+		 System.out.println("4567");
+		 System.out.println("DDDD");
+
+	}
+	public static void dev(){
+		System.out.println("分支");
+	}
+}
